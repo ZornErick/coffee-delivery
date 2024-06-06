603547e567156982d6a166b06ed619f56aee1ac2
erickzorn <erick.nogueira.zorn@gmail.com>
2024-06-06 18:26:51 -0300

    Estrutura básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import {BrowserRouter} from "react-router-dom";
+import {Router} from "./Router.tsx";
+
+import {ThemeProvider} from "styled-components";
+import {defaultTheme} from "./styles/themes/default.ts";
+import {GlobalStyle} from "./styles/global.ts";
+
 export function App() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <BrowserRouter>
+                <Router />
+            </BrowserRouter>
+            <GlobalStyle />
+        </ThemeProvider>
+    );
 }
